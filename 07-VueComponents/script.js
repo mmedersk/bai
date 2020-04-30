26474b0768c499a9ac4a0b7644720e91b452753a
Mikolaj Mederski <RWXG48@motorolasolutions.com>
2020-04-30 17:03:43 +0200

    mod7

---
+++ 07-VueComponents/script.js
@@ -0,0 +1,12 @@
+Vue.component('university',{
+    template : `
+    <div>
+        <h1>Cracow University of Economics</h1>
+        <p>31-510 Kraków, Rakowicka 27, POLAND</p>
+    </div>
+    `
+ });
+
+ var app = new Vue({
+    el: '#app'
+ });
